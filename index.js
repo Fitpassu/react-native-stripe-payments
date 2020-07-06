5ab9c6a84d6545e912e56b0cf111edb187231b59
Viktoras Laukevičius <viktoras.laukevicius@yahoo.com>
2020-07-06 13:53:04 +0300

    Implement JS part of lib

--- index.js
+++
@@ -1,5 +0,0 @@
-import { NativeModules } from 'react-native';
-
-const { StripePayments } = NativeModules;
-
-export default StripePayments;
---
+++ lib/index.tsx
@@ -0,0 +1,44 @@
+import { NativeModules } from 'react-native';
+
+const { StripePayments } = NativeModules;
+
+export interface InitParams {
+  publishingKey: string
+}
+
+export interface CardDetails {
+  number: string,
+  expMonth: number,
+  expYear: number,
+  cvc: string,
+}
+
+export interface PaymentResult {
+  id: string,
+  paymentMethodId: string,
+}
+
+export interface StripeError {
+  status: string;
+  message: string;
+}
+
+class Stripe {
+  _stripeInitialized = false
+
+  setOptions = (options: InitParams) => {
+    if (this._stripeInitialized) { return; }
+    StripePayments.init(options.publishingKey);
+    this._stripeInitialized = true;
+  }
+
+  confirmPayment(clientSecret: string, cardDetails: CardDetails): Promise<PaymentResult> {
+    return StripePayments.confirmPayment(clientSecret, cardDetails)
+  }
+
+  isCardValid(cardDetails: CardDetails): boolean {
+    return StripePayments.isCardValid(cardDetails) == true;
+  }
+}
+
+export default new Stripe();
