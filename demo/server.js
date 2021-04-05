9c5f8f83ea75c02c8710bc80ba380c3c6080174e
Viktoras Laukevičius <viktoras.laukevicius@gmail.com>
2021-04-05 12:50:17 +0300

    Create card saving setup intent on server

--- demo/server.js
+++ demo/server.js
@@ -15,6 +15,21 @@ const server = http.createServer(async (req, res) => {
       });
       res.writeHead(200, {'Content-Type': 'application/json'});
       return res.end(JSON.stringify(intent));
+    case "/save-card":
+      const customers = await stripe.customers.list({ limit: 1 });
+      var customer;
+      if (customers.data.length < 1) {
+        customer = await stripe.customers.create({
+          description: 'Single Test Customer',
+        });
+      } else {
+        customer = customers.data[0];
+      }
+      const setupIntent = await stripe.setupIntents.create({
+        customer: customer.id,
+      });
+      res.writeHead(200, {'Content-Type': 'application/json'});
+      return res.end(JSON.stringify(setupIntent));
     default:
       res.writeHead(404, { 'Content-Type': 'application/json' });
       return res.end();
