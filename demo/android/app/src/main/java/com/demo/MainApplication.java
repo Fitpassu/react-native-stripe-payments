c12b3db134f884660a45de1f63a7ea9f51792243
Migle Bucelyte <migle.bucelyte@outlook.com>
2021-04-05 21:02:16 +0300

    Setup android demo app

--- demo/android/app/src/main/java/com/demo/MainApplication.java
+++ demo/android/app/src/main/java/com/demo/MainApplication.java
@@ -12,6 +12,7 @@ import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 import com.demo.generated.BasePackageList;
+import com.fitpassu.stripepayments.StripePaymentsPackage;
 
 import org.unimodules.adapters.react.ReactAdapterPackage;
 import org.unimodules.adapters.react.ModuleRegistryAdapter;
@@ -43,6 +44,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       List<ReactPackage> packages = new PackageList(this).getPackages();
       packages.add(new ModuleRegistryAdapter(mModuleRegistryProvider));
+      packages.add(new StripePaymentsPackage());
       return packages;
     }
 
